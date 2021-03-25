43ea6bf993f1f6cf235309ebfd6d08f5cfe2887c
Virezox Zhu <virezox@gmail.com>
2021-03-25 22:50:49 +0800

    Add CharsetEncodingFilter

--- projects/stage-0/user-platform/user-web/src/main/java/sh/vitex/projects/user/web/filter/CharsetEncodingFilter.java
+++ projects/stage-0/user-platform/user-web/src/main/java/sh/vitex/projects/user/web/filter/CharsetEncodingFilter.java
@@ -1,4 +1,45 @@
 package sh.vitex.projects.user.web.filter;
 
-public class CharsetEncodingFilter {
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * CharsetEncodingFilter
+ */
+public class CharsetEncodingFilter implements Filter {
+
+    private String encoding = null;
+
+    private ServletContext servletContext;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.encoding = filterConfig.getInitParameter("encoding");
+        this.servletContext = filterConfig.getServletContext();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpRequest.setCharacterEncoding(encoding);
+            httpResponse.setCharacterEncoding(encoding);
+            servletContext.log("Current Charset has been setted to: " + encoding);
+            // CharsetEncodingFilter -> FrontControllerServlet -> forward -> index.jsp
+        }
+
+        // exectue filter chain
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
